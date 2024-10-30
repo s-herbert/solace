8773884305f1958c9dc9fc3804f06f1e6ac07a5b
sherbert <11893541+s-herbert@users.noreply.github.com>
2024-10-29 19:50:23 -0700

    oops forgot export

--- src/lib/getAdvocates.ts
+++ src/lib/getAdvocates.ts
@@ -1,7 +1,7 @@
 import db from "../db";
 import { advocates } from "../db/schema";
 
-const getAdvocatesDB = async () => {
+export const getAdvocatesDB = async () => {
   const data = await db.select().from(advocates);
   return Response.json({ data });
 }
